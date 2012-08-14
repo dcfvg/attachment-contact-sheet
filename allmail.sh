1afc83e5a0fcdcf6b952d08c0b165b2d933b0886
dcfvg <qwsx@dcfvg.com>
2012-08-14 20:15:33 +0200

    Cleaning

--- allmail.sh
+++ allmail.sh
@@ -1,38 +1,34 @@
 start_time=`date +%s`
-
 cd ~/Desktop
 
-# rm -r cachedir;
-# 
-# echo "Creation du cache …"
-# mkdir cachedir
-# 
-# echo "Recherche … "
-# find /Users/*/Library/Mail/V2 -name '*.jpg'   -exec cp {} cachedir \;
-# find /Users/*/Library/Mail/V2 -name '*.jpeg'  -exec cp {} cachedir \;
-# find /Users/*/Library/Mail/V2 -name '*.png'       -exec cp {} cachedir \;
-# #find /Users/*/Library/Mail/V2 -name '*.gif'  -exec cp {} cachedir \;
-# #find /Users/*/Library/Mail/V2 -name '*.pdf'  -exec cp {} cachedir \;
-# echo "images copiés …"
+rm -r cachedir;
+rm -r cachedirHD;
 
-#mogrify -thumbnail 200x200 -gravity center -crop 200x200+0+0 "cachedir/*"
-#echo "images réduites …"
+## debut du script
 
-#montage cachedir/* -mode Concatenate allmail.png
-#montage "cachedir/*" -geometry 200x200\>+0+0 all.png
+mkdir cachedir
+echo "Dossier de cache …"
+
+find /Users/*/Library/Mail/V2 -name '*.jpg' -exec cp {} cachedir \;
+find /Users/*/Library/Mail/V2 -name '*.jpeg'-exec cp {} cachedir \;
+find /Users/*/Library/Mail/V2 -name '*.png' -exec cp {} cachedir \;
+find /Users/*/Library/Mail/V2 -name '*.gif' -exec cp {} cachedir \;
+find /Users/*/Library/Mail/V2 -name '*.pdf' -exec cp {} cachedir \;
+echo "images copiés …"
+
+cp -r cachedir cachedirHD
+echo "cache dupliqué …"
 
 mogrify -thumbnail 200x200 "cachedir/*"
+echo "images réduites …"
 
 montage "cachedir/*" -auto-orient \
     -bordercolor white -background black +polaroid \
     -gravity center -background none -extent 0x0 \
-    -background none -geometry -50-50  pj.png
-
+    -background none -geometry -55-55  pj.png
 echo "montage terminé"
 
-#rm -r cachedir;
-#echo "cachedir supprimé";
-
+######
 end_time=`date +%s` 
 secs=$(($end_time-$start_time))
 echo "Temps total "$(($secs/3600))":"$(($secs%3600/60))":"$(($secs%60))
